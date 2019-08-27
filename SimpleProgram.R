c05245b75446ec5b1b006755ddf58f619984ec2a
Brian Kidd <kidd6@tamu.edu>
2019-08-27 18:16:47 -0500

    Added calculating OLS and MSE on data

--- SimpleProgram.R
+++ SimpleProgram.R
@@ -8,8 +8,14 @@ sigma = 2 # noise standard deviation
 beta = rep(2,p) # true vector of coefficients
 X = matrix(rnorm(n*p), n, p) # n by p matrix of predictors
 
+# Source FunctionLM.R to use the functions needed
+source("FunctionsLM.R")
+
 # [ToDo] Use generateY function to generate Y
+Y <- generateY(X,beta,sigma)
 
 # [ToDo] Use calculateBeta function to calculate beta_LS
+beta_LS <- calculateBeta(X,Y)
 
 # [ToDo] Use calculateMSE to assess the estimation error measured by squared eucledian distance - ||beta - beta_LS||_2^2
+est_error <- calculateMSE(beta, beta_LS)
